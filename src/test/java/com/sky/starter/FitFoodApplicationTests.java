a2f189be91e35b4435aa7d4df2e6739697e96bbc
sky_butterfly <sky_butterfly@naver.com>
2020-09-04 15:50:21 +0900

    Add Test Page

--- src/test/java/com/sky/starter/FitFoodApplicationTests.java
+++ src/test/java/com/sky/starter/FitFoodApplicationTests.java
@@ -1,13 +1,20 @@
 package com.sky.starter;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=FitFoodApplicationTests.class)
 class FitFoodApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void Test() {
+		System.out.println("Å×½ºÆ®");
+	}
 }
