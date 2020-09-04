e4f75304e53a41e5142b691738d839da216de912
sky_butterfly <sky_butterfly@naver.com>
2020-09-04 15:26:34 +0900

    Create Project

---
+++ src/main/java/com/fit/food/FitFoodApplication.java
@@ -0,0 +1,13 @@
+package com.fit.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitFoodApplication.class, args);
+	}
+
+}
